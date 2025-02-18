af6c8a52b52ed6721ef03a3e73af95776f6b7e70
OneCodeVector <codevectorprgm@gmail.com>
2025-02-17 22:41:56 -0500

    2/17/2025
    Fixed Update Image Boxes

--- Scripts/ImageBox.js
+++ Scripts/ImageBox.js
@@ -7,14 +7,14 @@ function UpdateImageBoxes()
         let LeftSided = (ImageRect.x < window.innerWidth / 2)
         let MoveDirection = LeftSided ? -1 : 1
 
-        let CalculatedDistance = Math.clamp(window.scrollY - ImageRect.y + ImageRect.height / 2.5, 0, (ImageRect.width + 25))
-        console.log(CalculatedDistance)
-        let DistanceMagnitude = Math.zig(CalculatedDistance / ImageRect.width)
-        let MoveDistance = Math.nzoSoftClamp(DistanceMagnitude / 2, 0, 1, 3.4) * ImageRect.width
+        let CalculatedDistance = Math.clamp(window.scrollY - ImageRect.y + ImageRect.height / 2.5, -1e3, (ImageRect.width + 25))
+        //console.log(CalculatedDistance)
+        let DistanceMagnitude = CalculatedDistance / ImageRect.width
+        let MoveDistance = Math.nzoSoftClamp(DistanceMagnitude / 2, 0, 1, 3.4) * ImageRect.width + 50
 
         ImageBox.style.left = `${LeftSided ? 
-            -(ImageRect.width + 25 - MoveDistance) : // Left-Side
-            ImageRect.width - MoveDistance + 25 // Right-Side
+            -(ImageRect.width - 25 - MoveDistance) : // Left-Side
+            ImageRect.width - MoveDistance - 25 // Right-Side
         }px`
     })
 }
