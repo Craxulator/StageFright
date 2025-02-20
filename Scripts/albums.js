f705d5ca7d66e6203a70c1abec8a4cb47b7d19ad
Andrews <craxulator@gmail.com>
2025-02-20 13:58:25 -0500

    2/20/2025

---
+++ Scripts/albums.js
@@ -0,0 +1,18 @@
+const Delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function Play(SongName)
+{
+
+}
+
+async function Play(SongName)
+{
+
+}
+
+async function Main()
+{
+
+}
+
+document.onreadystatechange = Main;
